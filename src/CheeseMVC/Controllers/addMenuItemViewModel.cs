f3333a5858b5d7b198408f45bc2ba7b3cd9eb8f3
gracebrandolph <56567886+gracebrandolph@users.noreply.github.com>
2020-05-05 23:31:23 -0500

    Finished with part 3

---
+++ src/CheeseMVC/Controllers/addMenuItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CheeseMVC.Models;
+using System.Collections.Generic;
+
+namespace CheeseMVC.Controllers
+{
+    internal class addMenuItemViewModel
+    {
+        private Menu theMenu;
+        private List<Cheese> cheeses;
+
+        public addMenuItemViewModel(Menu theMenu, List<Cheese> cheeses)
+        {
+            this.theMenu = theMenu;
+            this.cheeses = cheeses;
+        }
+    }
+}
